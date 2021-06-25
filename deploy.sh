04d707446a3bc16cee5131a3a77e67ac668a3669
Kevin Sham <ksham@ip-tribe.com>
2021-06-25 14:51:21 +0800

    travisci

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t kesyyyy/multi-client:latest -t kesyyyy/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t kesyyyy/multi-server:latest -t kesyyyy/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t kesyyyy/multi-worker:latest -t kesyyyy/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push kesyyyy/multi-client:latest
 docker push kesyyyy/multi-server:latest
 docker push kesyyyy/multi-worker:latest
@@ -8,7 +9,8 @@ docker push kesyyyy/multi-worker:latest
 docker push kesyyyy/multi-client:$SHA
 docker push kesyyyy/multi-server:$SHA
 docker push kesyyyy/multi-worker:$SHA
+
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=kesyyyy/multi-server:$SHA
-kubectl set image deployments/client-deployment server=kesyyyy/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=kesyyyy/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=kesyyyy/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=kesyyyy/multi-worker:$SHA
